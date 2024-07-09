a9782c91209023e634d71e4b6991078932ea2460
Austin Walker <austin@unstructured.io>
2024-07-09 16:26:44 -0400

    Update jest config to work with non barrel file imports
    We need to compile when testing to have access to all the `import {} from foo.js` lines.

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,16 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  silent: true,
+  preset: 'ts-jest/presets/default-esm', // or other ESM presets
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
 };
